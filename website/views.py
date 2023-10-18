663c71a126c10de0f7e474a0d39c88e543c247b1
j-lederer <125938397+j-lederer@users.noreply.github.com>
2023-10-18 13:42:21 +0000

    update 4

--- website/views.py
+++ website/views.py
@@ -236,8 +236,10 @@ def print_numbers_task(self, seconds):
           meta={'current': num, 'total': seconds, 'status': 'Printing'})
         progress = num/seconds #THIS CHANGES
         task = Task.query.get(self.request.id)
-        task.user.add_notification('task_progress', {'task_id': self.request.id,
-                                                     'progress': progress})
+      current_user.add_notification('task_progress', {'task_id': self.request.id,
+         'progress': progress})
+        # task.user.add_notification('task_progress', {'task_id': self.request.id,
+        #                                              'progress': progress})
         if progress >= 100:
             task.complete = True
         db.session.commit()
