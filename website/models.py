eb00ed5d9fd2931b2ec2825b221af38cf4b91762
j-lederer <125938397+j-lederer@users.noreply.github.com>
2023-11-20 23:27:40 +0000

    Final 11

--- website/models.py
+++ website/models.py
@@ -213,7 +213,7 @@ class My_task_tracker(db.Model):
 
 
 class History(db.Model):
-  id = db.Column(db.String(36), primary_key=True, nullable=False)
+  id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(128), index=True)
   description = db.Column(db.String(128))
   user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
