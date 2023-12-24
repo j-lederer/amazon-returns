f226765c6c1ae36e6f2a8eafc6b9e6d8ee695cba
j-lederer <125938397+j-lederer@users.noreply.github.com>
2023-12-24 06:24:37 +0000

    debug 21

--- website/amazonAPI.py
+++ website/amazonAPI.py
@@ -463,7 +463,7 @@ def increaseInventory(Quantity_of_SKUS, task_id, my_task_tracker_id, user_id, re
 def increaseInventory_all_jobs(Quantity_of_SKUS, task_id, my_task_trackers_ids_array, user_id, refresh_token):
   #set task status
   print("I am in increaseInventory_all_jobs()  AMAZONAPI     !!!!!!!")
-  result = []
+  result = {}
   result[0] = None
   try:
     task = Task.query.get(task_id)
