bbfd6346a151e4c2d49cc9369a7fd1f33b01272f
Jonathan Lederer <125938397+j-lederer@users.noreply.github.com>
2024-06-11 03:13:26 +0000

    Turn links into button

---
+++ website/static/home.js
@@ -0,0 +1,30 @@
+document.getElementById('clearSearchButton').addEventListener('click', function() {
+    window.location.href = '/clearSearch';
+});
+
+document.getElementById('refreshButton').addEventListener('click', function() {
+    window.location.href = '/refresh_returns_and_inventory';
+});
+
+
+document.getElementById('addToJobsButton').addEventListener('click', function() {
+    if (confirm('Add the queue to your jobs. The increase inventory operation will execute at 12:00 am ET every night.')) {
+        window.location.href = '/create_job';
+    }
+});
+
+document.getElementById('clearQueueButton').addEventListener('click', function() {
+    if (confirm('Are you sure you want to clear the queue?')) {
+        window.location.href = '/clearQueue';
+    }
+});
+
+document.getElementById('downloadPdfButton').addEventListener('click', function() {
+    var url = this.getAttribute('data-url');
+    window.location.href = url;
+});
+
+document.getElementById('downloadSlimButton').addEventListener('click', function() {
+    var url = this.getAttribute('data-url');
+    window.location.href = url;
+});
