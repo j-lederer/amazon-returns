f026820d1aa8b0cace8a38e4673e2326f257dd3e
Jonathan Lederer <125938397+j-lederer@users.noreply.github.com>
2024-06-11 06:40:06 +0000

    Fix addToJobs button

--- website/static/home.js
+++ website/static/home.js
@@ -29,16 +29,3 @@ document.getElementById('downloadSlimButton').addEventListener('click', function
     window.location.href = url;
 });
 
-// Get all the <h3> elements in intro and introAnswers
-var introH3s = document.getElementById('intro').querySelectorAll('h3');
-var introAnswersH3s = document.getElementById('introAnswers').querySelectorAll('h3');
-
-// Loop through each <h3> element in introAnswers
-introAnswersH3s.forEach(function(element, index) {
-    // Get the corresponding <h3> element in intro
-    var correspondingIntroH3 = introH3s[index];
-    // Get the height of the <h3> element in introAnswers
-    var height = element.offsetHeight;
-    // Set the height of the corresponding <h3> element in intro
-    correspondingIntroH3.style.height = height + 'px';
-});
