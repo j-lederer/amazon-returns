369732f0b3beb9b912fb4ba1f3292dd7ed473ef9
Jonathan Lederer <125938397+j-lederer@users.noreply.github.com>
2024-08-04 20:15:54 +0000

    another try

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from website import create_app
+from flask import Flask
 
 # app, celery= create_app()
 #Used to use this in Render for start command: celery -A main.celery worker -B --loglevel=info
 app = create_app()
-celery = app.extensions["celery"]
+# celery = app.extensions["celery"]
+from celery import Celery, Task
+def celery_init_app(app: Flask) -> Celery:
+    class FlaskTask(Task):
+        def __call__(self, *args: object, **kwargs: object) -> object:
+            with app.app_context():
+                return self.run(*args, **kwargs)
+
+    celery_app = Celery(app.name, task_cls=FlaskTask)
+    celery_app.config_from_object(app.config["CELERY"])
+    celery_app.set_default()
+    app.extensions["celery"] = celery_app
+    return celery_app
+
+celery = celery_init_app(app)
 
 if __name__ == '__main__':
   
--- website/__init__.py
+++ website/__init__.py
@@ -112,22 +112,24 @@ def create_app():
   
   # celery = make_celery(app)
   # celery.set_default()
-  from celery import Celery, Task
-  def celery_init_app(app: Flask) -> Celery:
-    class FlaskTask(Task):
-        def __call__(self, *args: object, **kwargs: object) -> object:
-            with app.app_context():
-                return self.run(*args, **kwargs)
 
-    celery_app = Celery(app.name, task_cls=FlaskTask)
-    celery_app.config_from_object(app.config["CELERY"])
-    celery_app.set_default()
-    app.extensions["celery"] = celery_app
-    return celery_app
+    
+  # from celery import Celery, Task
+  # def celery_init_app(app: Flask) -> Celery:
+  #   class FlaskTask(Task):
+  #       def __call__(self, *args: object, **kwargs: object) -> object:
+  #           with app.app_context():
+  #               return self.run(*args, **kwargs)
 
-  # celery_app = celery_init_app(app)
-  app.config.from_prefixed_env()
-  celery_init_app(app)
+  #   celery_app = Celery(app.name, task_cls=FlaskTask)
+  #   celery_app.config_from_object(app.config["CELERY"])
+  #   celery_app.set_default()
+  #   app.extensions["celery"] = celery_app
+  #   return celery_app
+
+    
+  # app.config.from_prefixed_env()
+  # celery_init_app(app)
 
   
   from .models import User, Role
