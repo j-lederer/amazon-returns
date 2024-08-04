3f090a977fa0e59784cd6df274cd318f5f8f63c9
Jonathan Lederer <125938397+j-lederer@users.noreply.github.com>
2024-08-04 17:56:20 +0000

    more celery edits

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from website import create_app
 # app, celery= create_app()
 #Used to use this in Render for start command: celery -A main.celery worker -B --loglevel=info
 app = create_app()
+celery_app = app.extensions["celery"]
 
 if __name__ == '__main__':
   
