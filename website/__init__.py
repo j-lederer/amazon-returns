18deed671d1e880f7fd5fc194b5bcb2831d33ec0
Jonathan Lederer <125938397+j-lederer@users.noreply.github.com>
2024-08-05 03:41:20 +0000

    try closer to documentation celery

--- website/__init__.py
+++ website/__init__.py
@@ -106,22 +106,22 @@ def create_app():
   # celery.set_default()
 
     
-  # from celery import Celery, Task
-  # def celery_init_app(app: Flask) -> Celery:
-  #   class FlaskTask(Task):
-  #       def __call__(self, *args: object, **kwargs: object) -> object:
-  #           with app.app_context():
-  #               return self.run(*args, **kwargs)
+  from celery import Celery, Task
+  def celery_init_app(app: Flask) -> Celery:
+    class FlaskTask(Task):
+        def __call__(self, *args: object, **kwargs: object) -> object:
+            with app.app_context():
+                return self.run(*args, **kwargs)
 
-  #   celery_app = Celery(app.name, task_cls=FlaskTask)
-  #   celery_app.config_from_object(app.config["CELERY"])
-  #   celery_app.set_default()
-  #   app.extensions["celery"] = celery_app
-  #   return celery_app
+    celery_app = Celery(app.name, task_cls=FlaskTask)
+    celery_app.config_from_object(app.config["CELERY"])
+    celery_app.set_default()
+    app.extensions["celery"] = celery_app
+    return celery_app
 
     
-  # app.config.from_prefixed_env()
-  # celery_init_app(app)
+  app.config.from_prefixed_env()
+  celery_init_app(app)
 
   
   from .models import User, Role
